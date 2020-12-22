75f6b395437f88170718d8c213494d34f9cadff6
Evan1992 <280132947@qq.com>
2020-12-21 20:54:23 -0600

    added source code

---
+++ Two-Dimensional Convolution/public.h
@@ -0,0 +1,6 @@
+#ifndef _PARAMETERS_H
+#define _PARAMETERS_H
+
+typedef struct {float r; float i;} complex;
+
+#endif
---
+++ Two-Dimensional Convolution/utilities.c
@@ -0,0 +1,24 @@
+#include "public.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int N;
+void read_from_file(char* file_name, complex (*im)[N]){
+    FILE *f; /*open file descriptor */
+    f = fopen(file_name, "r");
+    int i, j;
+    for (i=0;i<512;i++)
+        for (j=0;j<512;j++)
+            fscanf(f,"%g",&im[i][j].r);
+    fclose(f);
+}
+
+void write_to_file(char *file_name, complex (*im)[N]){
+    FILE *f;
+    f = fopen(file_name, "w");
+    int i, j;
+    for(i=0; i<512; i++)
+        for(j=0; j<512; j++)
+            fprintf(f, "%16.7e", im[i][j].r);
+        fprintf(f, "\n");
+}
